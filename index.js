5ef819aae3411aba9b01f1cde7be5b40f77d82f0
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2022-10-24 15:20:08 -0700

    calculate sum of input date

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const dateOfBirth=document.querySelector('#date-of-birth')
 const luckyNumber=document.querySelector('#lucky-number')
 const checkNumberButton=document.querySelector('#check-number')
 
+function calculateSum(dob) {
+    dob = dob.replaceAll("-","")
+    let sum = 0;
+    for (let index = 0; index < dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
+    
+    }
+    return sum;
+}
+
 checkNumberButton.addEventListener('click',function getValues() {
-    console.log(dateOfBirth.value,luckyNumber.value);
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob)
+    console.log(sum);
 })
