2175f8a00edd9c6597f35899d46dcf7c0a42bc13
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2022-10-24 15:36:40 -0700

    console if birthday is lucky or not

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ const dateOfBirth=document.querySelector('#date-of-birth')
 const luckyNumber=document.querySelector('#lucky-number')
 const checkNumberButton=document.querySelector('#check-number')
 
+
+function compareValues(sum,number) {
+    if(sum%number===0)
+    {
+        console.log("Your Birthday is Lucky");
+    }
+    else
+    {
+        console.log("Your Birthday is not Lucky");
+    }
+}
+
 function calculateSum(dob) {
     dob = dob.replaceAll("-","")
     let sum = 0;
     for (let index = 0; index < dob.length; index++) {
         sum = sum + Number(dob.charAt(index));
-    
     }
     return sum;
 }
 
 checkNumberButton.addEventListener('click',function getValues() {
     const dob = dateOfBirth.value;
+    const number = luckyNumber.value;;
     const sum = calculateSum(dob)
-    console.log(sum);
+    compareValues(sum,number)
 })
